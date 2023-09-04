5baa90cb0201177a2339d2dde7730c7fcbc27373
przemekgrzesiak91 <przemek.grzesiak91@gmail.com>
2023-09-04 13:39:21 +0200

    First view

--- dna_app/apps.py
+++ dna_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DnaAnalyzerAppConfig(AppConfig):
+class DnaAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DNA_Analyzer_app'
+    name = 'dna_app'
