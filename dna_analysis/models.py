8318b082f4304feb88de2b88e4346f78546c1334
przemekgrzesiak91 <przemek.grzesiak91@gmail.com>
2023-09-14 11:44:15 +0200

    Fresh django project, with models in dna_analysis app

---
+++ dna_analysis/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class DNASequence(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    sequence = models.TextField()
+    length = models.PositiveIntegerField()
+
+    # stosunek G/C
+    # typ sekwencji?
+    # w zasadzie czytanie jej czyli wklej sekwencej (FASTa/ itp) i rozloz ja na czesci i uzupełni pola.)?
+
