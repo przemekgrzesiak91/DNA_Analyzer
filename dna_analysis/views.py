06481137fe280f9a8eca1057203c9fe7fa59c90d
przemekgrzesiak91 <przemek.grzesiak91@gmail.com>
2023-09-14 13:33:34 +0200

    modified templates to display projects from db

--- dna_analysis/views.py
+++ dna_analysis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
 
 def project_list(request):
-    return render(request, 'dna_analysis/project_list.html', {})
+    projects = Project.objects.all()
+    return render(request, 'dna_analysis/project_list.html', {'projects': projects})
