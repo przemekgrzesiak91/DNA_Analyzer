62cf86fbaff8e824ebc3cfafc81bbcab2986192b
przemekgrzesiak91 <przemek.grzesiak91@gmail.com>
2023-09-28 11:22:50 +0200

    added form - new project

--- dna_analysis/views.py
+++ dna_analysis/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 
@@ -10,3 +12,15 @@ def project_list(request):
 def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
     return render(request, 'dna_analysis/project_detail.html', {'project': project})
+
+def project_new(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.author = request.user
+            project.save()
+            return redirect('project_detail', pk=project.pk)
+    else:
+        form = ProjectForm()
+    return render(request, 'dna_analysis/project_edit.html', {'form': form})
