c2335d35d2f4c2b380f7d94e437c08c5e4e752bc
przemekgrzesiak91 <przemek.grzesiak91@gmail.com>
2023-10-05 11:37:42 +0200

    Added login and logout function

--- dna_analysis/urls.py
+++ dna_analysis/urls.py
@@ -1,8 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.views.generic.base import TemplateView  # new
+
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('', views.project_list, name='project_list'),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('register/', views.register, name='register'),
+
+
+    path('project_list', views.project_list, name='project_list'),
     path('project/<int:pk>/', views.project_detail, name='project_detail'),
     path('project/new/', views.project_new, name='project_new'),
     path('project/<int:pk>/edit/', views.project_edit, name='project_edit'),
